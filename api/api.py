8462ef70af3373c5d735664616c5e6925d148889
Sam Elamin <hussam.elamin@gmail.com>
2021-09-02 13:29:52 +0100

    make swagger docs the root

--- api/api.py
+++ api/api.py
@@ -2,7 +2,7 @@ from flask_restx import Api
 from api.errors.error_handler import errors
 
 api = Api(
-    doc='/swagger',
+    doc='/',
     version='1.0.0',
     title='kylin-api',
     description="Kylin Network's API providing price feed data from various sources (bancor, coinbase, coingecko, cryptocompare, cryptowatch, etc)",
